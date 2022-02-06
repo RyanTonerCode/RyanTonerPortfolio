06f2db69fb32cb6e9ea2e81c688273d3f0331934
Ryan Toner <ryan.toner@student.fairfield.edu>
2022-02-06 04:31:01 -0700

    .NET 6

--- RyanTonerPortfolio/Models/ErrorViewModel.cs
+++ RyanTonerPortfolio/Models/ErrorViewModel.cs
@@ -1,10 +1,8 @@
-using System;
-
 namespace RyanTonerPortfolio.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
